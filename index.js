447aebb367b35bd60386e065b437c86b9a873416
Serena Hasler <srhasler@yahoo.com>
2020-01-29 13:48:02 -0600

    Add examples of let and count.

---
+++ index.js
@@ -0,0 +1,16 @@
+//PRIMITIVE DATA
+
+//We need a way to reuse of access data, so we create a REFERENCE
+//'const' alerts JS to reserve some space in memory
+//With 'const' I cannot reassign the value in memory; If unsure use 'const'
+//We provide a name for this space in memory
+//We use the ASSIGNMENT OPERATOR '='
+//This takes the VALUE on the right and assigns to the NAMED REFERENCE.
+const myString = "string";
+
+//Use 'let' ONLY if we plan to REASSIGN the VALUE REFERENCED by the VARIABLE.
+let myNum = 23;
+
+//myString = "something else";
+
+console.log(myNum);
