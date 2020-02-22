3af535e8b383eb4dc2219bdc1c54c71aa06b96d8
Serena Hasler <srhasler@yahoo.com>
2020-02-22 16:39:07 -0600

    test

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-//PRIMITIVE DATA
+console.log(4 === 4);
+console.log("Hello, world" === "Hello, world");
 
-//We need a way to reuse of access data, so we create a REFERENCE
-//'const' alerts JS to reserve some space in memory
-//With 'const' I cannot reassign the value in memory; If unsure use 'const'
-//We provide a name for this space in memory
-//We use the ASSIGNMENT OPERATOR '='
-//This takes the VALUE on the right and assigns to the NAMED REFERENCE.
-const myString = "string";
+let x = false;
+let y = x;
+console.log(x === y);
+x = !x;
+console.log(x === y);
 
-//Use 'let' ONLY if we plan to REASSIGN the VALUE REFERENCED by the VARIABLE.
-let myNum = 23;
+console.log([1, 2, "c"] === [1, 2, "c"]);
+console.log({} === {});
 
-//myString = "something else";
-
-console.log(myNum);
-
-//Homework due 2/3/2020 lines 19-26
-const hwString = "words";
-const hwNumber = 524;
-const hwBoo = true;
-
-let hwLetNum = 626;
-
-hwLetNum = 867; //let allows me to change hwLetNum's assigned value
-hwString = "nope"; //const does NOT allow me to change the variable's value
+y = { id: 1 };
+x = y;
+console.log(x === y);
+y.id += 1;
+console.log(x.id === y.id);
