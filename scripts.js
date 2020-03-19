d157945c11d43ced77e2f8d750b78973619ead3e
Serena Hasler <srhasler@yahoo.com>
2020-03-18 21:18:43 -0500

    HW due 3.25 Question 1

--- scripts.js
+++ scripts.js
@@ -5,26 +5,3 @@ const greeting = function() {
 };
 
 greeting("Morning", "Louisa");
-
-// TODO: Update 👆🏽by using `prompt` to get the user's name.
-
-const greeting2 = function() {
-  console.log(`${arguments["0"]}`);
-};
-
-let who = prompt("What is your name?");
-
-greeting2("Morning " + who);
-
-// TODO: Create an OBJECT 'greeterDB' that will 🏠 the 'data' as per 👆🏽.
-
-const greeterDB = {};
-
-greeterDB.greeting = "Morning";
-greeterDB.who = prompt("What is your name?");
-
-const greeterDB2 = function() {
-  console.log(`${arguments["0"]}`);
-};
-
-greeterDB2(greeterDB.greeting + " " + greeterDB.who);
