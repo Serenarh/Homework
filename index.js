e70555065086fea962a2ac0539fcb1f4c969c114
Serena Hasler <srhasler@yahoo.com>
2020-03-31 14:49:09 -0500

    HW due 4.02 Add numberSummer function with comments

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-//Sum the #s in this array
-const nums = [51, 22, 33, 44, 11, 65, 7, 8, 9, 10]; //declare, create, and fill array with integers
-
-//function will sum the integers in an array passed to the function
-function sumDaNumbers(numbers) {
-  let total = 0; //set total variable to 0
-  for (let i = 0; i < numbers.length; i++) {
-    //set up the for loop; i is the iterator and is set to 0; i will stop after it hits the end of the array (since it is set to one less than the array's length & will represent each elements' index; i will go up by 1 each time the loop runs )
-    total += numbers[i]; //loop through the array one at a time
+//numberSummer is a function which will sum the numbers between and including a starting number & an ending number, both of which will be passed to the function
+function numberSummer(start, end) {
+  let total = 0; //the total begins at 0
+  //i is the iterator--it begins at the number passed into start;  the loop will stop after i has reached the ending number passed into end; i increases by one with each loop iteration
+  for (let i = start; i <= end; i++) {
+    total += i; //the total increases by the number represented by i
   }
-  return total; //return the total of the array's integer elements
+  return total; //the total number is returned
 }
 
-const totalOfNums = sumDaNumbers(nums); //create a variable to hold result of array nums
-//being passed into sumDaNumbers
-console.log(totalOfNums); //print the total returned from sumDaNumbers--rep by var totalOfNums
-
-console.log(sumDaNumbers(nums)); //here print total returned from sumDaNumbers by passing nums directly into the function
+console.log(numberSummer(1, 10)); //the console will print the total returned by numberSummer when 1 is passed to the function as the starting number (start) and 10 is passed to the function as the ending number (end)
