ee2a6ccaf053c60b15ef3671b2ac5018bb17c455
Serena Hasler <srhasler@yahoo.com>
2020-04-22 10:32:06 -0500

    Add variables

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const respond = document.querySelector("form");
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const title = document.querySelector("#title");
+const period = document.querySelector("#period");
+const genre = document.querySelector("#genre");
