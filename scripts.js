3a27a54e10633ceff829f984b1bb4f8a89a9492b
Serena Hasler <srhasler@yahoo.com>
2020-04-22 10:33:02 -0500

    Add setters to submit event

--- scripts.js
+++ scripts.js
@@ -4,3 +4,11 @@ const lastName = document.querySelector("#lastname");
 const title = document.querySelector("#title");
 const period = document.querySelector("#period");
 const genre = document.querySelector("#genre");
+
+respond.addEventListener("submit", e => {
+  e.preventDefault();
+
+  document.querySelector(
+    "p"
+  ).textContent = `Your book is ${title.value} which was written by ${firstName.value} ${lastName.value}.\n The author wrote during the ${period.value} period in the genre of ${genre.value}.`;
+});
